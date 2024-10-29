49d43a441b0c1ddf77f6aca66822be7cbc5cc918
Srivyshnavi-K <srivyshnavik10@gmail.com>
2024-10-29 19:31:25 +0530

    Add files via upload

---
+++ discovery/src/test/java/com/learnings/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnings.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
